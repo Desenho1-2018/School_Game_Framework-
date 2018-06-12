51c5250d62567e38c0db03c5adda4593ffdac527
tayh <tayhcarvalho@gmail.com>
2018-06-12 14:15:32 +0100

    Creating CollectLevel and fixing some things

---
+++ School_Game_Framework/src/game/CollectLevel.java
@@ -0,0 +1,29 @@
+package game;
+
+public class CollectLevel extends TemplateCollectLevel{
+
+	@Override
+	protected void initializeObjects() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initializeKeyboard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void drawLevel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String runScenario() {
+		// TODO Auto-generated method stub
+		return nextScenario;
+	}
+
+}
--- School_Game_Framework/src/game/TemplateCollectLevel.java
+++ School_Game_Framework/src/game/TemplateCollectLevel.java
@@ -1,14 +1,16 @@
 package game;
 
-public abstract class TemplateCollectLevel {
-	public final void runScnario() {
+public abstract class TemplateCollectLevel extends Level{
+	public final void initializeLevel() {
 		initializeObjects();
 		initializeKeyboard();
 		drawLevel();
+		runScenario();
 	}
 	
 	protected abstract void initializeObjects();
 	protected abstract void initializeKeyboard();
 	protected abstract void drawLevel();	
+	public abstract String runScenario();	
 	
 }
