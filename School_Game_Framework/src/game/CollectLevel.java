eec8b922659d8f8170486b8bf3578521cac5b18d
tayh <tayhcarvalho@gmail.com>
2018-06-12 15:45:40 +0100

    Implementing some methods

--- School_Game_Framework/src/game/CollectLevel.java
+++ School_Game_Framework/src/game/CollectLevel.java
@@ -1,23 +1,41 @@
 package game;
 
-public class CollectLevel extends TemplateCollectLevel{
+import jplay.GameImage;
+import jplay.Keyboard;
+import jplay.Window;
+
+public class CollectLevel extends TemplateCollectLevel {
+
+	private String scenarioName;
+	private GameObject object;
+
+	public CollectLevel(Window gameWindow, String name) {
+		this.window = gameWindow;
+		this.scenarioName = name;
+	}
 
 	@Override
 	protected void initializeObjects() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	protected void initializeKeyboard() {
-		// TODO Auto-generated method stub
-		
+		if (window != null) {
+			sceneKeyboard = window.getKeyboard();
+		} else {
+			System.out.println("The keyboard needs a window to run. The window cannot be null");
+		}
+		sceneKeyboard.setBehavior(Keyboard.DOWN_KEY, Keyboard.DETECT_EVERY_PRESS);
+		sceneKeyboard.setBehavior(Keyboard.UP_KEY, Keyboard.DETECT_EVERY_PRESS);
+		sceneKeyboard.setBehavior(Keyboard.LEFT_KEY, Keyboard.DETECT_EVERY_PRESS);
+		sceneKeyboard.setBehavior(Keyboard.RIGHT_KEY, Keyboard.DETECT_EVERY_PRESS);
 	}
 
 	@Override
 	protected void drawLevel() {
-		// TODO Auto-generated method stub
-		
+		while (nextScenario == null) {
+			background.draw();
+		}
 	}
 
 	@Override
@@ -25,5 +43,7 @@ public class CollectLevel extends TemplateCollectLevel{
 		// TODO Auto-generated method stub
 		return nextScenario;
 	}
+
+
 
 }
--- School_Game_Framework/src/game/TemplateCollectLevel.java
+++ School_Game_Framework/src/game/TemplateCollectLevel.java
@@ -1,16 +1,20 @@
 package game;
 
-public abstract class TemplateCollectLevel extends Level{
+import jplay.GameImage;
+
+public abstract class TemplateCollectLevel extends Level {
 	public final void initializeLevel() {
 		initializeObjects();
 		initializeKeyboard();
 		drawLevel();
 		runScenario();
 	}
-	
+
 	protected abstract void initializeObjects();
+
 	protected abstract void initializeKeyboard();
-	protected abstract void drawLevel();	
-	public abstract String runScenario();	
-	
+
+	protected abstract void drawLevel();
+
+	public abstract String runScenario();
 }
