45dfd877edb7bbe6719165017a60436a5cb6a4e0
IolaneAndrade <andrade.icaa@gmail.com>
2018-06-14 10:22:13 -0300

    start Score

---
+++ School_Game_Framework/src/game/Score.java
@@ -0,0 +1,37 @@
+package game;
+
+public class Score {
+	private String player = " ";
+	private String fase = " ";
+	private String mencao = " ";
+	private String password = " ";
+	
+	
+	public String getPlayer() {
+		return player;
+	}
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+	public String getFase() {
+		return fase;
+	}
+	public void setFase(String fase) {
+		this.fase = fase;
+	}
+	public String getMencao() {
+		return mencao;
+	}
+	public void setMencao(String mencao) {
+		this.mencao = mencao;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
