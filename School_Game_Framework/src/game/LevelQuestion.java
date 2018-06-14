fa3fa0c3156a565cac76bea42a634cc0698aa729
Lucas Souza <lucas.soaresouza@gmail.com>
2018-06-14 13:50:33 -0300

    Initializing the question level generalization.

---
+++ School_Game_Framework/src/game/LevelQuestion.java
@@ -0,0 +1,10 @@
+package game;
+
+import java.util.ArrayList;
+
+public class LevelQuestion extends Level{
+
+	private String statement; //Enunciado da questao
+	private ArrayList<String> alternatives; //Alternativas da questao
+	
+}
