ac499ad92fff803c5f3b2aa6420ba24ff95b7a86
Lucas Souza <lucas.soaresouza@gmail.com>
2018-06-15 12:01:21 -0300

    Adding a structure of levelQuestion
    Co-authored-by: Kleber Moreira <kleberbritomoreira10@gmail.com>

--- School_Game_Framework/src/game/LevelQuestion.java
+++ School_Game_Framework/src/game/LevelQuestion.java
@@ -2,11 +2,50 @@ package game;
 
 import java.util.ArrayList;
 
+import jplay.Window;
+
 public class LevelQuestion extends Level{
 
 	private String statement; //Enunciado da questao
 	private ArrayList<Alternative> alternatives; //Alternativas da questao
 	
-	public LevelQuestion(){};
+	public LevelQuestion(Window gameWindow, String name){
+		
+		window = gameWindow;
+		scenarioName = name;
+		
+	};
+	
+	protected void updateScanario() {
+		
+		while(nextScenario == null) {
+			
+			drawObjects();
+			window.update();
+			
+		}
+		
+	}
+	
+	protected void initializeKeyboard() {
+		//TO DO
+	}
+
+	public String runScenario() {
+		
+		initializeKeyboard();
+		updateScenario();
+		//this.playerInstance.setPreviousLevel("XABLAU");
+		return nextScenario;
+		
+	}
+	
+	private void waitATime() {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
