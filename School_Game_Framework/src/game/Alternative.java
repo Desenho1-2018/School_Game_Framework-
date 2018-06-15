d3c859afc42c8e1f7f71383ab915762169758300
Mateus Roriz <mateusvroriz6b@gmail.com>
2018-06-15 11:46:51 -0300

    Creating a Alternative class for alternatives of questions levels.
    Co-authored-by: Stéfane Souza <stefanesouza04@gmail.com>

---
+++ School_Game_Framework/src/game/Alternative.java
@@ -0,0 +1,22 @@
+package game;
+
+public class Alternative {
+
+	private int number = 0;
+	private String alternative;
+	private boolean validate;
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public String getAlternative() {
+		return alternative;
+	}
+	
+	public boolean getValidate() {
+		return validate;
+	}
+	
+	
+}
--- School_Game_Framework/src/game/LevelQuestion.java
+++ School_Game_Framework/src/game/LevelQuestion.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class LevelQuestion extends Level{
 
 	private String statement; //Enunciado da questao
-	private ArrayList<String> alternatives; //Alternativas da questao
+	private ArrayList<Alternative> alternatives; //Alternativas da questao
+	
+	public LevelQuestion(){};
 	
 }
