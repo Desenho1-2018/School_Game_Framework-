9d9f35f51c2c7d1f1edae721c31f1a33cc1c0fd8
Emanuel <manusenac@hotmail.com>
2018-06-16 16:52:31 -0300

    change class name

--- School_Game_Framework/src/game/question.java
+++ School_Game_Framework/src/game/question.java
@@ -4,7 +4,7 @@
 	public class question2  extends  GameObject{
 		boolean resposta;
 
-		public question(int x, int y, String spritePath) {
+		public question2(int x, int y, String spritePath) {
 			super(x,y, spritePath,1,GameObjectType.OTHER);
 			// TODO Auto-generated constructor stub
 		}
